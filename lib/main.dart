c4d44fcd1aaa1e6d7cbb0e06e1206a984fad0632
wael4code <121891714+wael4code@users.noreply.github.com>
2023-03-12 18:09:48 +0200

    add theme , create view viewNote

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:noteapp/views/viewNotes.dart';
 
 void main() {
-  runApp(noteApp());
+  runApp(const noteApp());
 }
+
 
 class noteApp extends StatelessWidget {
   const noteApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      theme: ThemeData(brightness: Brightness.dark),
+      home: const viewNotes(),
+    );
   }
 }
