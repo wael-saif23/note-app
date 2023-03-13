45c70fc119a87bfcb068724ef2e84aaaf5b86ad5
wael4code <121891714+wael4code@users.noreply.github.com>
2023-03-13 23:16:52 +0200

    init hive and open note box

--- lib/contants.dart
+++ lib/contants.dart
@@ -1,3 +1,4 @@
 import 'package:flutter/material.dart';
 
 const kPrimaryColor = Color(0xff53EBD4);
+const kNoteBox = "note_box";
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/adapters.dart';
+import 'package:noteapp/contants.dart';
 import 'package:noteapp/views/viewNotes.dart';
 
-void main() {
+void main()async {
+
+  await Hive.initFlutter();
+  await Hive.openBox(kNoteBox);
   runApp(const noteApp());
 }
 
