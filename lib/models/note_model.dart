230671e0505dce0bee71bffdd4f23265c5d1ad3c
wael4code <121891714+wael4code@users.noreply.github.com>
2023-03-13 22:19:55 +0200

    create note model

---
+++ lib/models/note_model.dart
@@ -0,0 +1,12 @@
+class NoteModel {
+  final String title;
+  final String subtitle;
+  final String dare;
+  final int color;
+
+  NoteModel(
+      {required this.title,
+      required this.subtitle,
+      required this.dare,
+      required this.color});
+}
