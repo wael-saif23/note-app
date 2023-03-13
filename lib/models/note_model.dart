594deb84169121096e501162b048cb27f75ea5b6
wael4code <121891714+wael4code@users.noreply.github.com>
2023-03-13 22:58:28 +0200

    create type aduptor to note model

--- lib/models/note_model.dart
+++ lib/models/note_model.dart
@@ -1,7 +1,18 @@
-class NoteModel {
+import 'package:hive/hive.dart';
+
+part 'note_model.g.dart';
+
+@HiveType(typeId: 0)
+class NoteModel extends HiveObject{
+
+  
+  @HiveField(0)
   final String title;
+  @HiveField(1)
   final String subtitle;
+  @HiveField(2)
   final String dare;
+  @HiveField(3)
   final int color;
 
   NoteModel(
