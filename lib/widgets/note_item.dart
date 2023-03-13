bf9977815182dd9b1ac1fc9b048cb690bc5654a9
wael4code <121891714+wael4code@users.noreply.github.com>
2023-03-13 18:03:43 +0200

    add edit note view

--- lib/widgets/note_item.dart
+++ lib/widgets/note_item.dart
@@ -1,57 +1,66 @@
 import 'package:flutter/material.dart';
 
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:noteapp/views/editNoteView.dart';
 
 class noteItem extends StatelessWidget {
   const noteItem({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-          color: Color.fromARGB(255, 184, 131, 88),
-          borderRadius: BorderRadius.circular(16)),
-      child: Padding(
-        padding: const EdgeInsets.only(top: 24, bottom: 24, left: 14),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.end,
-          children: [
-            ListTile(
-              title: Text(
-                "Flutter Tips",
-                style: TextStyle(
-                  fontSize: 24,
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => editNoteView()),
+        );
+      },
+      child: Container(
+        decoration: BoxDecoration(
+            color: Color.fromARGB(255, 184, 131, 88),
+            borderRadius: BorderRadius.circular(16)),
+        child: Padding(
+          padding: const EdgeInsets.only(top: 24, bottom: 24, left: 14),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              ListTile(
+                title: Text(
+                  "Flutter Tips",
+                  style: TextStyle(
+                    fontSize: 24,
+                    color: Colors.black,
+                  ),
+                ),
+                subtitle: Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 16),
+                  child: Text(
+                    "Thanks Mr.tharwat samy for your course, Wael Saif.",
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.black.withOpacity(.6),
+                    ),
+                  ),
+                ),
+                trailing: IconButton(
+                  onPressed: () {},
+                  icon: Icon(FontAwesomeIcons.trash),
                   color: Colors.black,
+                  iconSize: 24,
                 ),
               ),
-              subtitle: Padding(
-                padding: const EdgeInsets.symmetric(vertical: 16),
+              Padding(
+                padding: const EdgeInsets.only(right: 24),
                 child: Text(
-                  "Thanks Mr.tharwat samy for your course, Wael Saif.",
+                  "Mars 13 , 2023",
                   style: TextStyle(
+                    color: Colors.black.withOpacity(0.6),
                     fontSize: 20,
-                    color: Colors.black.withOpacity(.6),
                   ),
                 ),
               ),
-              trailing: IconButton(
-                onPressed: () {},
-                icon: Icon(FontAwesomeIcons.trash),
-                color: Colors.black,
-                iconSize: 24,
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(right: 24),
-              child: Text(
-                "Mars 13 , 2023",
-                style: TextStyle(
-                  color: Colors.black.withOpacity(0.6),
-                  fontSize: 20,
-                ),
-              ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
